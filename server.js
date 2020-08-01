597ee6e073e4c63206137142fc29235a05b35782
MehtaManan07 <f20190507@pilani.bits-pilani.ac.in>
2020-08-01 18:49:10 +0530

    setup of routes and database

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
-const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
+const express = require("express");
+const dotenv = require("dotenv");
+const colors = require('colors');
+const morgan = require("morgan");
 
 // Load env vars
 dotenv.config({ path: './server/config/config.env' })
 
+const connectDB = require("./server/config/db"); // connect database
 const app = express()
+
+app.use('/api/v1',require("./server/routes/Bootcamps"))
+
 const PORT = process.env.PORT || 5000
 app.listen(PORT, console.log(`Server running on port ${PORT}`))
