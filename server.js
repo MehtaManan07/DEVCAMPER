94a36256d1298ae865de2e188f3cd8031e77f429
MehtaManan07 <f20190507@pilani.bits-pilani.ac.in>
2020-08-01 19:01:22 +0530

    added required folders

--- server.js
+++ server.js
@@ -1,15 +1,27 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const colors = require('colors');
+const colors = require("colors");
 const morgan = require("morgan");
 
 // Load env vars
-dotenv.config({ path: './server/config/config.env' })
+dotenv.config({ path: "./server/config/config.env" });
 
-const connectDB = require("./server/config/db"); // connect database
-const app = express()
+const connectDB = require("./server/config/db"); // load database
+const app = express();
 
-app.use('/api/v1',require("./server/routes/Bootcamps"))
+connectDB();
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT, console.log(`Server running on port ${PORT}`))
+// Dev logging middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+app.use("/api/v1", require("./server/routes/Bootcamps"));
+
+const PORT = process.env.PORT || 5000;
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${port}`.blue.bold
+  )
+);
