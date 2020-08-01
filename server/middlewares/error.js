5ff15d05ebe76914b085240a20cb23c7216f0d38
MehtaManan07 <f20190507@pilani.bits-pilani.ac.in>
2020-08-02 00:19:06 +0530

    ErrorResponse class used in middleware

--- server/middlewares/error.js
+++ server/middlewares/error.js
@@ -1,8 +1,18 @@
+const ErrorResponse = require("../utils/errorRes");
+
 const errorHandler = (err,req,res,next) => {
+    let error = { ...err }
+    error.message = err.message;
     // Log to console...
     console.log(err.stack.red)
 
-    res.status(err.statusCode || 500).json({
+    // Mongoose bad object
+    if(err.name === "CastError") {
+        const message = `Resource not found with id of ${err.value}`;
+        error = new ErrorResponse(message,404)
+    }
+
+    res.status(error.statusCode || 500).json({
         success: false,
         error: err.message || "Server Error"
     })
