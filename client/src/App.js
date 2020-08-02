6d5c1357eb28b7a400db6103dbfa7b052534c246
MehtaManan07 <f20190507@pilani.bits-pilani.ac.in>
2020-08-02 20:05:21 +0530

    Added routing requirements

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import './App.css';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <Switch>
+      </Switch>
     </div>
   );
 }
