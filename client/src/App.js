1351d999cfb758d2826d9268ec7f825e7d83b244
MehtaManan07 <f20190507@pilani.bits-pilani.ac.in>
2020-08-02 20:34:47 +0530

    Added navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import NavbarComponent from './components/core/Navbar';
 import Landing from './pages/Landing';
 
 function App() {
   return (
     <div>
+    <NavbarComponent />
       <Switch>
       <Route exact path="/" component={Landing} />
       </Switch>
