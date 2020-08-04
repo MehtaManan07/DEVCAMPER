3b26a675c19702013464649fcec54caf90d48799
MehtaManan07 <f20190507@pilani.bits-pilani.ac.in>
2020-08-04 21:27:49 +0530

    Controller for getting courses

---
+++ server/controllers/course.js
@@ -0,0 +1,23 @@
+const Course = require("../models/Course");
+const ErrorResponse = require("../utils/errorRes");
+const asyncHandler = require("../middlewares/async");
+
+// @desc      Get courses
+// @route     POST /api/v1/courses
+// @route     POST /api/v1/bootcamps/:bootcampId/courses
+// @access    Private
+
+exports.getCourses = asyncHandler(async (req, res, next) => {
+  let query;
+  if (req.params.bootcampId) {
+    query = Course.find({ bootcamp: req.params.bootcampId });
+  } else {
+    query = Course.find();
+  }
+  const courses = await query;
+  res.status(200).json({
+    data: courses,
+    count: courses.length,
+    success: true,
+  });
+});
