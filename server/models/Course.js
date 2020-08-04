a534fab65690e93a9d82b75cb6be10e68048fb63
MehtaManan07 <f20190507@pilani.bits-pilani.ac.in>
2020-08-04 21:09:00 +0530

    Added course model

---
+++ server/models/Course.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const courseSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    trim: true,
+    required: [true, "Please add a course title"],
+  },
+  description: {
+    type: String,
+    required: [true, "PLease add a course description"],
+  },
+  weeks: {
+    type: String,
+    required: [true, "PLease add number of weeks"],
+  },
+  tuition: {
+    type: Number,
+    required: [true, "PLease add a tuition cost"],
+  },
+  tuition: {
+    type: String,
+    required: [true, "PLease add a minimum skill"],
+    enum: ["beginner", "intermediate", "advanced"]
+  },
+  scholarshipAvailable: {
+    type: Boolean,
+    default: false
+  },
+  date: {
+      type: Date,
+      default: Date.now
+  },
+  bootcamp: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Bootcamp",
+      required: true
+  }
+});
+const Course = mongoose.model("Course",courseSchema)
+module.exports = Course
