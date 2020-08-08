4dfe21ddf2f72c169b48595d45a8b97cc1bfdf69
MehtaManan07 <f20190507@pilani.bits-pilani.ac.in>
2020-08-08 16:34:32 +0530

    hashing password and saving user in database

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -32,6 +33,13 @@ const userSchema = new mongoose.Schema({
       default: Date.now
   }
 });
+
+// Encrypting password;
+userSchema.pre("save", async function(next) {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password,salt);
+  
+})
 
 const User = mongoose.model('User', userSchema)
 module.exports = User
