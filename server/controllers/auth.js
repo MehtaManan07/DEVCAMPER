1f0b77cf820865a1949dde7f7bbc3efb1d4d8731
MehtaManan07 <f20190507@pilani.bits-pilani.ac.in>
2020-08-08 18:41:06 +0530

    getting user by id

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -62,6 +62,5 @@ const sendTokenResponse = (user, statusCode, res) => {
 
 exports.getMe = asyncHandler(async (req, res, next) => {
   const user = await User.findById(req.user.id);
-
   res.status(200).json({ success: true, data: user });
 });
