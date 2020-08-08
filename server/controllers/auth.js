23392f24aa07cfb498aa203344298498f9546a62
MehtaManan07 <f20190507@pilani.bits-pilani.ac.in>
2020-08-08 16:53:25 +0530

    generating token  and saving user in database

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -11,5 +11,7 @@ exports.register = asyncHandler(async (req, res, next) => {
 
   // Create a user
   const user = await User.create({ name, email, password, role });
-  res.status(201).json({ success: true, user })
+  //create token;
+  const token = user.getSignedJwtToken()
+  res.status(201).json({ success: true, token })
 });
